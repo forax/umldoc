99536ba8c10992ca7926bd7ada602aa363bc09ed
Thomas <velu.thomas77@laposte.net>
2022-10-04 14:04:55 +0200

    added veluvexiau package

---
+++ src/main/java/com/github/veluvexiau/umldoc/core/SubtypeDependency.java
@@ -0,0 +1,10 @@
+package com.github.veluvexiau.umldoc.core;
+
+/**
+ * Subtype dependency.
+ *
+ * @param supertype the supertype of the dependency
+ * @param subtype the subtype of the dependency
+ */
+public record SubtypeDependency(Entity supertype, Entity subtype) implements Dependency {
+}
