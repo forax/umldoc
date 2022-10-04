67684fe6828e9b4684db76f08d645c4d1fd15b8c
forax <forax@univ-mlv.fr>
2022-10-04 15:28:22 +0200

    fix: should support a set of modifiers

--- src/main/java/com/github/forax/umldoc/core/Entity.java
+++ src/main/java/com/github/forax/umldoc/core/Entity.java
@@ -2,14 +2,15 @@ package com.github.forax.umldoc.core;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * An entity (a class, an interface, a record, etc).
  *
- * @param modifier the entity modifier
+ * @param modifiers the entity modifier
  * @param stereotype a stereotype if it exists
  * @param fields the fields of the entity
  * @param methods the methods of the entity
  */
-public record Entity(Modifier modifier, Optional<String> stereotype,
+public record Entity(Set<Modifier> modifiers, Optional<String> stereotype,
                      List<Field> fields, List<Method> methods) { }
