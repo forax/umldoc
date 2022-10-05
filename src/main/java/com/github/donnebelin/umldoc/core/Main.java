3ee57cd9b8b41d351deeec86f2b38dfadb5defe2
forax <forax@univ-mlv.fr>
2022-10-05 17:39:35 +0200

    add a println otherwise spotbugs fails the build

--- src/main/java/com/github/donnebelin/umldoc/core/Main.java
+++ src/main/java/com/github/donnebelin/umldoc/core/Main.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         var entities = new JarParser().parse();
+        System.out.println(entities);
     }
 }
