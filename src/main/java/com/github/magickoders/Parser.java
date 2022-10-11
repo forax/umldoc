2dc18ea5631b16dd51c7a20f69de687c165e64e4
brian babillon <brian.babillon@gmail.com>
2022-10-11 18:29:25 +0200

    add missing javadoc

--- src/main/java/com/github/magickoders/Parser.java
+++ src/main/java/com/github/magickoders/Parser.java
@@ -2,6 +2,9 @@ package com.github.magickoders;
 
 import com.github.forax.umldoc.core.Package;
 
+/**
+ * Abstraction of a parser of Package to String.
+ */
 public interface Parser {
   String parse(Package p);
 }
