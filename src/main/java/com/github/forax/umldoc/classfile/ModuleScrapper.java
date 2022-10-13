a701c5649248c38df1faaac1e31d75b46e464541
forax <forax@univ-mlv.fr>
2022-10-13 08:34:07 +0200

    refactor so the code is a little more readable

--- src/main/java/com/github/forax/umldoc/classfile/ModuleScrapper.java
+++ src/main/java/com/github/forax/umldoc/classfile/ModuleScrapper.java
@@ -1,12 +1,14 @@
 package com.github.forax.umldoc.classfile;
 
+import static java.util.stream.Collectors.groupingBy;
+
 import com.github.forax.umldoc.core.Entity;
 import com.github.forax.umldoc.core.Package;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.lang.module.ModuleReference;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 /**
@@ -22,6 +24,22 @@ public final class ModuleScrapper {
     return index == -1 ? "" : entityName.substring(0, index);
   }
 
+  private static void parseModule(ModuleReference moduleReference, Consumer<Entity> consumer) {
+    try (var reader = moduleReference.open()) {
+      for (var filename : (Iterable<String>) reader.list()::iterator) {
+        if (!filename.endsWith(".class") || filename.endsWith("-info.class")) {
+          continue;
+        }
+        try (var inputStream = reader.open(filename).orElseThrow()) {
+          var entity = ClassFileParser.parseClass(inputStream);
+          consumer.accept(entity);
+        }
+      }
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
   /**
    * Returns a list of packages contained in the module reference taken as argument.
    *
@@ -32,22 +50,8 @@ public final class ModuleScrapper {
   public static List<Package> scrapModule(ModuleReference moduleReference) throws IOException {
     try {
       return Stream.of(moduleReference)
-          .<Entity>mapMulti((module, consumer) -> {
-            try (var reader = moduleReference.open()) {
-              for (var filename : (Iterable<String>) reader.list()::iterator) {
-                if (!filename.endsWith(".class") || filename.endsWith("-info.class")) {
-                  continue;
-                }
-                try (var inputStream = reader.open(filename).orElseThrow()) {
-                  var entity = ClassFileParser.parseClass(inputStream);
-                  consumer.accept(entity);
-                }
-              }
-            } catch (IOException e) {
-              throw new UncheckedIOException(e);
-            }
-          })
-          .collect(Collectors.groupingBy(entity -> packageName(entity.name())))
+          .mapMulti(ModuleScrapper::parseModule)
+          .collect(groupingBy(entity -> packageName(entity.name())))
           .entrySet().stream()
           .map(entry -> new Package(entry.getKey(), entry.getValue(), List.of()))
           .toList();
