be076bca4129463d79d3cc789b5774dc728feb44
Leo Tostain <leo.tostain@gmail.com>
2022-10-15 14:26:02 +0200

    add generation of fields anb associations

--- src/main/java/com/github/pereiratostain/generator/MermaidSchemaGenerator.java
+++ src/main/java/com/github/pereiratostain/generator/MermaidSchemaGenerator.java
@@ -3,20 +3,29 @@ package com.github.pereiratostain.generator;
 import static java.util.Objects.requireNonNull;
 
 import com.github.forax.umldoc.core.Entity;
+import com.github.forax.umldoc.core.Field;
 import com.github.forax.umldoc.core.Modifier;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Generate a schema in the Mermaid format.
  */
 public class MermaidSchemaGenerator implements Generator {
+  private HashSet<String> entitiesName;
 
   @Override
   public void generate(Writer writer, List<Entity> entities) throws IOException {
     requireNonNull(writer);
     requireNonNull(entities);
+
+    entitiesName = new HashSet<>(entities.stream().map(Entity::name).collect(Collectors.toSet()));
 
     generateHeader(writer);
     for (var entity : entities) {
@@ -33,10 +42,90 @@ public class MermaidSchemaGenerator implements Generator {
   }
 
   private void generateEntity(Writer writer, Entity entity) throws IOException {
+    var associations = new ArrayList<String>();
+    var stereotype = "";
+    String fields;
+
+    if(entity.stereotype() == Entity.Stereotype.ENUM) {
+      fields = computeFieldsEnum(entity);
+      stereotype = "\t<<enumeration>>\n";
+    } else {
+      fields = computeFieldsClass(entity, associations);
+    }
+
     writer.append("    class ")
             .append(entity.name())
-            .append(" {")
-            .append("\n    }\n\n");
+            .append(" {\n")
+            .append(stereotype)
+            .append(fields)
+            .append("\n    }\n")
+            .append(generateAssociations(entity, associations))
+            .append("\n");
+  }
+
+  private String computeFieldsEnum(Entity entity) {
+    var fields = new ArrayList<Field>();
+
+    for(var field: entity.fields()) {
+      if(!field.name().equals("$VALUES")) {
+        fields.add(field);
+      }
+    }
+    return generateRecordFields(fields);
+  }
+
+  private String computeFieldsClass(Entity entity, ArrayList<String> associations) {
+    var fields = new ArrayList<Field>();
+    Pattern pattern = Pattern.compile("<.*>");
+
+    for(var field: entity.fields()) {
+      var fieldType = field.type();
+      fieldType = fieldType.replace(";", "");
+
+      Matcher matcher = pattern.matcher(fieldType);
+
+      if(matcher.find()) {
+        var string = matcher.group(0);
+        string = string.replace("<", "");
+        string = string.replace(">", "");
+        fieldType = string;
+      }
+
+      if(entitiesName.contains(fieldType)) {
+        associations.add(fieldType);
+      } else {
+        fields.add(field);
+      }
+    }
+
+    return generateFields(fields);
+  }
+
+  private String generateFields(List<Field> fields) {
+    var string = new StringBuilder();
+
+    for (var field: fields) {
+      string.append("\t").append(modifierToString(field.modifiers().iterator().next())).append(field.name()).append(" : ").append(field.type().replace(";", "")).append("\n");
+    }
+    return string.toString();
+  }
+
+  private String generateRecordFields(List<Field> fields) {
+    var string = new StringBuilder();
+
+    for (var field: fields) {
+      string.append("\t").append(field.name()).append("\n");
+    }
+    return string.toString();
+  }
+
+  private String generateAssociations(Entity entity, List<String> associations) {
+    var string = new StringBuilder();
+
+    for (var field: associations) {
+      string.append("\t").append(entity.name()).append(" --> ").append(field).append('\n');
+    }
+    return string.toString();
   }
 
   private static String modifierToString(Modifier modifier) {
