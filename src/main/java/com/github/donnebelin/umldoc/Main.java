ca1b79200a9b7cf983a7a64598dfd9dd60348a04
AxelBelin <axel.belin07@gmail.com>
2022-10-18 12:19:26 +0200

    Add field and associations support for entities.

---
+++ src/main/java/com/github/donnebelin/umldoc/Main.java
@@ -0,0 +1,58 @@
+package com.github.donnebelin.umldoc;
+
+import com.github.donnebelin.umldoc.classfile.JarParser;
+import com.github.donnebelin.umldoc.gen.MermaidGenerator;
+import com.github.donnebelin.umldoc.gen.PlantUmlGenerator;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Contains the main method.
+ * for this package
+ */
+public class Main {
+  /**
+   * main method of this package.
+   *
+   * @param args The path of the file
+   *             for which the program will write
+   *             Plant UML and Mermaid UML diagrams
+   * @throws IOException if I/O exception
+   *                     occurred during parsing
+   */
+  public static void main(String[] args)
+          throws IOException {
+    var parser = new JarParser();
+//    var packages = parser.resolvePackages();
+    var entities = parser.entities()
+            .stream()
+            .filter(entity -> entity.name().contains("forax_umldoc"))
+            .toList();
+    var filePath = Path.of(args[0]);
+    var plantUmlGenerator = new PlantUmlGenerator();
+    var mermaidGenerator = new MermaidGenerator();
+
+    try (var writer =
+                 Files.newBufferedWriter(filePath,
+                         StandardCharsets.UTF_8)
+    ) {
+      plantUmlGenerator.generate(
+              true,
+              entities,
+              parser.getAssociationDependencies(),
+              writer);
+      writer.append('\n')
+              .append('\n')
+              .append("```mermaid")
+              .append('\n');
+      mermaidGenerator.generate(
+              true,
+              entities,
+              parser.getAssociationDependencies(),
+              writer);
+      writer.append("```");
+    }
+  }
+}
---
+++ src/main/java/com/github/donnebelin/umldoc/gen/PlantUmlGenerator.java
@@ -0,0 +1,63 @@
+package com.github.donnebelin.umldoc.gen;
+
+import com.github.donnebelin.umldoc.Helper;
+import com.github.forax.umldoc.core.AssociationDependency;
+import com.github.forax.umldoc.core.Entity;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Generate a class diagram using the plantuml format.
+ */
+public final class PlantUmlGenerator implements Generator {
+  @Override
+  public void generate(boolean header, List<Entity> entities, List<AssociationDependency> dependencies,
+                       Writer writer) throws IOException {
+    requireNonNull(entities);
+    requireNonNull(dependencies);
+    requireNonNull(writer);
+    if (header) {
+      writer.append("""
+          @startuml
+          
+          """);
+    }
+
+    for (var dependency : dependencies) {
+      writer.append("""
+              %s "%s" -->  "%s" %s : %s
+              """.formatted(dependency.left().entity().name(),
+              Helper.parseCardinalities(dependency.left().cardinality()),
+              Helper.parseCardinalities(dependency.right().cardinality()),
+              dependency.right().entity().name(),
+              dependency.right().label().orElse("Not defined")
+      ));
+    }
+
+    for (var entity : entities) {
+      writer.append("""
+              class %s {
+                %s
+              }
+
+          """.formatted(
+                  entity.name(),
+                  entity.fields()
+                          .stream()
+                          .map(Generator::fieldToString)
+                          .collect(Collectors.joining("\n\t\t\t"))
+      ));
+    }
+
+    if (header) {
+      writer.append("""
+          @enduml
+          """);
+    }
+  }
+}
