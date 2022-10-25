ffa47bce7bb83200aae5746a87caa756d48acafd
AxelBelin <axel.belin07@gmail.com>
2022-10-25 15:03:15 +0200

    Fix checkstyle errors.

--- src/main/java/com/github/donnebelin/umldoc/classdiagram/DiagramFormater.java
+++ src/main/java/com/github/donnebelin/umldoc/classdiagram/DiagramFormater.java
@@ -9,57 +9,79 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * This class handles the associations and the fields.
+ * It built the class diagram
+ */
 public class DiagramFormater {
   private final List<Entity> entities;
-//  private final HashMap<String, Set<String>> mappedEntitiesToPackage;
 
+
+  /**
+   * Create a new Diagram formatter from a list of entities.
+   *
+   * @param entities the list of entities of the class diagram.
+   */
   public DiagramFormater(List<Entity> entities) {
     Objects.requireNonNull(entities);
     this.entities = entities;
-//    this.mappedEntitiesToPackage = mapPackageToEntities(entities);
 
   }
 
   private boolean entityNameExist(String entityName) {
-    return entities.stream().anyMatch(entity -> getEntityNameWithoutPackage(entity.type().name()).equals(getEntityNameWithoutPackage(entityName)));
+    return entities.stream().anyMatch(entity ->
+            getEntityNameWithoutPackage(entity.type().name())
+                    .equals(getEntityNameWithoutPackage(entityName)));
   }
 
   private Optional<Entity> getEntityByName(String name) {
-    return entities.stream().filter(entity -> getEntityNameWithoutPackage(entity.type().name()).equals(name)).findFirst();
+    return entities.stream().filter(entity ->
+            getEntityNameWithoutPackage(entity.type().name())
+                    .equals(name)).findFirst();
   }
 
+  /**
+   * Supply a list with computed dependencies between all entities of the same package.
+   *
+   * @return A list of AssociationDependency instances.
+   */
   public List<AssociationDependency> createAssociationDependencies() {
     var associationList = new ArrayList<AssociationDependency>();
     for (var entity : entities) {
-      for(var field : entity.fields()){
-        var newAssociation = createAssociation(field,entity);
+      for (var field : entity.fields()) {
+        var newAssociation = createAssociation(field, entity);
         newAssociation.ifPresent(associationList::add);
       }
     }
     
     return associationList.stream()
-        .filter(associationDependency -> !associationDependency.left().entity().type().name().equals(associationDependency.right().entity().type().name()))
+        .filter(associationDependency ->
+                !associationDependency.left().entity().type().name()
+                        .equals(associationDependency.right().entity().type().name())
+        )
         .toList();
   }
 
-  private Optional<AssociationDependency> createAssociation(Field field, Entity entityLeft){
-    var fieldTypeInfo = parseEntityFieldToType(entityLeft,field);
-    if(fieldTypeInfo.isPresent()) {
-      var cardinality = getCardinality(fieldTypeInfo.get());
+  private Optional<AssociationDependency> createAssociation(Field field, Entity entityLeft) {
+    var fieldTypeInfo = parseEntityFieldToType(entityLeft, field);
+    if (fieldTypeInfo.isPresent()) {
+      var cardinality = getCardinality();
       var left = new AssociationDependency.Side(entityLeft, Optional.empty(), false, cardinality);
       var rightEntity = getEntityByName(getEntityNameWithoutPackage(fieldTypeInfo.get().name()));
-      if(rightEntity.isPresent()){
-        var right = new AssociationDependency.Side(rightEntity.get(), Optional.empty(), true, cardinality);
+      if (rightEntity.isPresent()) {
+        var right = new AssociationDependency.Side(
+                rightEntity.get(), Optional.empty(), true, cardinality
+        );
         return Optional.of(new AssociationDependency(left, right));
       }
     }
     return Optional.empty();
   }
 
-  private boolean entityAsSamePackage(String entityName1, String entityName2){
+  private boolean entityAsSamePackage(String entityName1, String entityName2) {
     var entity1 = getEntityByName(getEntityNameWithoutPackage(entityName1));
     var entity2 = getEntityByName(getEntityNameWithoutPackage(entityName2));
-    if(entity1.isEmpty() || entity2.isEmpty()){
+    if (entity1.isEmpty() || entity2.isEmpty()) {
       return false;
     }
     return getPackageName(entity1.get()).equals(getPackageName(entity2.get()));
@@ -70,9 +92,15 @@ public class DiagramFormater {
     return packageWithName.substring(0, packageWithName.lastIndexOf("/"));
   }
 
+  /**
+   * Get the name of the entity without the package name.
+   *
+   * @param entityNameWithPackage entity name with the package name
+   * @return The name of the entity without package name.
+   */
   public static String getEntityNameWithoutPackage(String entityNameWithPackage) {
     var index = entityNameWithPackage.lastIndexOf("/");
-    if(index < 0){
+    if (index < 0) {
       //Already no package then return them
       return entityNameWithPackage;
     }
@@ -81,7 +109,7 @@ public class DiagramFormater {
 
   private Optional<TypeInfo> parseEntityFieldToType(Entity left, Field field) {
     var type = field.typeInfo();
-    if(entityAsSamePackage(left.type().name(),type.name())){
+    if (entityAsSamePackage(left.type().name(), type.name())) {
       if (entityNameExist(getEntityNameWithoutPackage(type.name()))) {
         return Optional.of(type);
       }
@@ -89,8 +117,8 @@ public class DiagramFormater {
     return Optional.empty();
   }
 
-  private AssociationDependency.Cardinality getCardinality(TypeInfo typeInfo) {
-    return typeInfo.outer().isPresent() ? AssociationDependency.Cardinality.MANY : AssociationDependency.Cardinality.ONLY_ONE;
+  private AssociationDependency.Cardinality getCardinality() {
+    return AssociationDependency.Cardinality.MANY;
   }
 
 }
