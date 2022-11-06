78bc451f771fc2d15e44c728925ca7916f74fb85
tvelu77 <velu.thomas77@laposte.net>
2022-11-06 17:08:44 +0100

    added enum for the type of uml

--- src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
+++ src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
@@ -6,6 +6,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+enum UMLType {
+  PLANTUML,
+  MERMAID
+}
+
 /**
  *
  * A class used to read markdown file.
