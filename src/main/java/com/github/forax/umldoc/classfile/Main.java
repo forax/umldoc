7c1459fe6ff57c4eb3cc34b7f30ea7576c94245d
Leo Tostain <leo.tostain@gmail.com>
2022-11-06 18:52:49 +0100

    Read and write file

--- src/main/java/com/github/forax/umldoc/classfile/Main.java
+++ src/main/java/com/github/forax/umldoc/classfile/Main.java
@@ -5,8 +5,6 @@ import java.nio.file.Paths;
 
 public class Main {
   public static void main (String[] args) throws IOException {
-    var pathToFile = Paths.get("EntityBuilder.java");
-    System.out.println(pathToFile.toAbsolutePath());
     var file = Reader.checkFileExtension(Paths.get("design/test.md"));
     file.readFile();
   }
--- src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
+++ src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
@@ -3,19 +3,20 @@ package com.github.forax.umldoc.classfile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
-import java.util.stream.Stream;
 
+/*
 /**
- *
- * Enum to define what type of uml the user wants.
- *
- */
+*
+* Enum to define what type of uml the user wants.
+*
+
 enum UMLType {
   PLANTUML,
   MERMAID
 }
-
+*/
 /**
  *
  * A class used to read markdown file.
@@ -23,7 +24,6 @@ enum UMLType {
  */
 public final class MarkdownReader implements Reader {
   private final Path path;
-  private int umlAnnotationLineNumber;
 
   public MarkdownReader(Path path) {
     Objects.requireNonNull(path);
@@ -36,9 +36,23 @@ public final class MarkdownReader implements Reader {
    * @throws IOException If the file is not accessible or doesn't exist.
    */
   public void readFile() throws IOException {
-    Stream<String> stream = Files.lines(path);
-    stream.map(String::trim)
-      .filter(l -> !l.isEmpty())
-      .forEach(System.out::println);
+    try (var file = Files.newBufferedReader(path)) {
+      System.out.println("allo ?");
+      // input the (modified) file content to the StringBuffer "input"
+      var inputBuffer = new StringBuilder();
+      String line;
+
+      while ((line = file.readLine()) != null) {
+        System.out.println(line);
+        inputBuffer.append(line);
+        inputBuffer.append('\n');
+      }
+      // write the new string with the replaced line OVER the same file
+      var fileOut = Files.newOutputStream(Paths.get("rendu.md"));
+      fileOut.write(inputBuffer.toString().getBytes());
+      fileOut.close();
+    } catch (Exception e) {
+      throw new IOException("Problem reading file.");
+    }
   }
 }
