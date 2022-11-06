622aca46407e521906b62fd0a0be9b8303ab4d6f
tvelu77 <velu.thomas77@laposte.net>
2022-11-06 16:07:17 +0100

    created markdownreader to be able to read files using nio library

---
+++ src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
@@ -0,0 +1,28 @@
+package com.github.forax.umldoc.classfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+enum UMLType {
+  PLANTUML,
+  MERMAID
+}
+
+public class MarkdownReader {
+
+  public static void main (String[] args) throws IOException {
+    readFile(Paths.get("YOUR_TEST_FILE.txt"));
+  }
+
+  public static void readFile(Path path) throws IOException {
+    var classLoader = MarkdownReader.class.getClassLoader();
+    var inputStream = classLoader.getResourceAsStream("test");
+    Stream<String> stream = Files.lines(path);
+    stream.map(String::trim)
+      .filter(l -> !l.isEmpty())
+      .forEach(System.out::println);
+  }
+}
