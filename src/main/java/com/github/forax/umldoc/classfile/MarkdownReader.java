122718fcc0cce0f4fdc68ca4cde9f5632e4f89be
tvelu77 <velu.thomas77@laposte.net>
2022-11-06 16:53:54 +0100

    added an interface to create multiple reader in the future added javadoc for the methods

--- src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
+++ src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
@@ -2,27 +2,44 @@ package com.github.forax.umldoc.classfile;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-enum UMLType {
-  PLANTUML,
-  MERMAID
-}
+/**
+ *
+ * A class used to read markdown file.
+ *
+ */
+public final class MarkdownReader implements Reader {
 
-public class MarkdownReader {
-
+  /**
+   *
+   * A main class to test.
+   *
+   * @deprecated
+   * NOTE : USED FOR TESTING ONLY (to be removed !).
+   *
+   * @param args Arguments for the main.
+   */
+  @Deprecated
   public static void main (String[] args) {
     try {
-      readFile(Paths.get("YOUR_TEST_FILE.txt"));
+      var markdownReader = new MarkdownReader();
+      markdownReader.readFile(Paths.get("YOUR_TEST_FILE.md"));
     } catch (IOException e) {
       System.out.println("Be sure that the file is accessible or created !");
     }
   }
 
-  public static void readFile(Path path) throws IOException {
+  /**
+   *
+   * Read a md file and detect the annotation umldoc.
+   *
+   * @param path File's path.
+   * @throws IOException If the file is not accessible or doesn't exist.
+   */
+  public void readFile(Path path) throws IOException {
     var classLoader = MarkdownReader.class.getClassLoader();
     var inputStream = classLoader.getResourceAsStream("test");
     Stream<String> stream = Files.lines(path);
