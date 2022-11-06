2c93893834361b7b03044fb675ea0e2dfbd4fbc1
tvelu77 <velu.thomas77@laposte.net>
2022-11-06 18:00:22 +0100

    removed useless import

--- src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
+++ src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
@@ -3,7 +3,6 @@ package com.github.forax.umldoc.classfile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 /**
