fadf402386bfd40fd74d81c1ed4f7341d621cf95
brian babillon <brian.babillon@gmail.com>
2022-11-07 19:25:46 +0100

    extract getFileExtension method

--- src/main/java/com/github/forax/umldoc/classfile/Main.java
+++ src/main/java/com/github/forax/umldoc/classfile/Main.java
@@ -1,12 +1,26 @@
 package com.github.forax.umldoc.classfile;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-  public static void main (String[] args) throws IOException {
+
+  public static String getFileExtension(Path path) {
+    var fileName = path.getFileName()
+                       .toString();
+    var dotPos = fileName.lastIndexOf('.');
+    if (dotPos < 0) {
+      throw new IllegalArgumentException(
+              "The file does not contain an extension in the name " + fileName);
+    }
+    return fileName.substring(dotPos + 1);
+  }
+
+  public static void main(String[] args) throws IOException {
     var file = Reader.checkFileExtension(Paths.get("design/test.md"));
     file.readFile();
   }
+  
 }
 
