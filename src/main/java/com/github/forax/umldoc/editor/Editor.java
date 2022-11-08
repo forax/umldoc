9e1135a565790a81037c17f044e25f038a5e5167
Thomas <velu.thomas77@laposte.net>
2022-11-08 14:22:25 +0100

    created class

---
+++ src/main/java/com/github/forax/umldoc/editor/Editor.java
@@ -0,0 +1,11 @@
+package com.github.forax.umldoc.editor;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Editor {
+
+  public void edit(BufferedWriter writer, BufferedReader reader ) {
+
+  }
+}
