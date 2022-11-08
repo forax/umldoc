e3ec48a1b0792de5dd0c967e2349304ffe023a38
Thomas <velu.thomas77@laposte.net>
2022-11-08 08:48:38 +0100

    fixed a bug

--- src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
+++ src/main/java/com/github/forax/umldoc/classfile/MarkdownReader.java
@@ -6,12 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/*
 /**
 *
 * Enum to define what type of uml the user wants.
 *
-
+ /*
 enum UMLType {
   PLANTUML,
   MERMAID
