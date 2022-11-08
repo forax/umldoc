c46341cd187e8ac27967814ccaed5d0be0bff147
Adrien COSTANDI <adrien.costandi@gmail.com>
2022-11-08 15:16:08 +0100

    Revert "starting association"
    This reverts commit 80ab59a8945c1bb7b7380c33de3d42b61a0e64a5.

--- src/test/java/com/github/magickoders/jar/JarReaderTest.java
+++ src/test/java/com/github/magickoders/jar/JarReaderTest.java
@@ -1,22 +1,38 @@
 package com.github.magickoders.jar;
 
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 public class JarReaderTest {
 
-    @Test
-    public void testGetEntitiesNotNull() {
-        try {
-            var entities = JarReader.getEntities(Path.of("target", "classes"));
-            System.out.println(entities);
-            assertNotNull(entities);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  // FIXME
+//  @Test
+//  public void listNotNull() throws IOException {
+//    var entities = JarReader.getEntities(Path.of("target/classes"))
+//                            .stream()
+//                            .filter(entity -> entity.name()
+//                                                    .contains("Dependency"))
+//                            .toList();
+//
+//    assertAll(
+//            () -> assertNotNull(entities),
+//            () -> assertFalse(entities.isEmpty())
+//    );
+//  }
+//
+//  @Test
+//  public void entitiesOk() throws IOException {
+//    var entities = JarReader.getEntities(Path.of("target/classes"))
+//                            .stream()
+//                            .filter(entity -> entity.name()
+//                                                    .contains("Entity"))
+//                            .toList();
+//
+//    var entity = entities.stream()
+//                         .filter(e -> e.name()
+//                                       .substring(e.name()
+//                                                   .lastIndexOf(".") + 1)
+//                                       .contains("Entity"))
+//                         .findFirst()
+//                         .orElseThrow();
+//    assertEquals(Set.of(Modifier.PUBLIC, Modifier.FINAL), entity.modifiers());
+//    assertEquals(Stereotype.RECORD, entity.stereotype());
+//  }
 }
