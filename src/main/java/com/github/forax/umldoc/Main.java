6e65e31894c52f95fc6819de185f5cc18117a1e4
Leo Tostain <leo.tostain@gmail.com>
2022-11-08 15:22:26 +0100

    Adapt main for Editor

--- src/main/java/com/github/forax/umldoc/Main.java
+++ src/main/java/com/github/forax/umldoc/Main.java
@@ -1,9 +1,15 @@
 package com.github.forax.umldoc;
 
 import com.github.forax.umldoc.classfile.ModuleScrapper;
+import com.github.forax.umldoc.editor.Editor;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.lang.module.ModuleFinder;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 
 /**
  * Javadoc for Checkstyle.
@@ -16,10 +22,12 @@ public class Main {
    * @throws IOException dcscsqw
    */
   public static void main(String[] args) throws IOException {
-    var path = Path.of("target/classes");
-    var finder = ModuleFinder.of(path);
-    for (var moduleReference : finder.findAll()) {
-      ModuleScrapper.scrapModule(moduleReference);
-    }
+    var reader = Files.newBufferedReader(Path.of("design/test.md"));
+    var writer = Files.newBufferedWriter(Path.of("design/test.md"));
+    var config = new HashMap<String, CommandLineParser>();
+    config.put("mermaid", new MermaidCommandLineParser());
+    config.put("plantuml", new PlantCommandLineParser());
+    var editor = new Editor(config);
+    editor.edit(writer, reader);
   }
 }
