06afc7519ce4d75c2c23dc704e12b73687007bf8
marius <mamardon3@gmail.com>
2022-11-08 01:17:25 +0100

    Map methods call to the related method

--- src/main/java/com/github/forax/umldoc/classfile/ClassFileParser.java
+++ src/main/java/com/github/forax/umldoc/classfile/ClassFileParser.java
@@ -1,23 +1,15 @@
 package com.github.forax.umldoc.classfile;
 
-import static com.github.forax.umldoc.core.AssociationDependency.Cardinality.MANY;
-import static com.github.forax.umldoc.core.AssociationDependency.Cardinality.ONLY_ONE;
-import static com.github.forax.umldoc.core.AssociationDependency.Cardinality.ZERO_OR_ONE;
 import static java.util.Objects.requireNonNull;
 import static org.objectweb.asm.Opcodes.ASM9;
 
-import com.github.forax.umldoc.core.AssociationDependency.Cardinality;
 import com.github.forax.umldoc.core.Call;
 import com.github.forax.umldoc.core.Method;
 import com.github.forax.umldoc.core.TypeInfo;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Consumer;
-import java.util.stream.Stream;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
@@ -92,66 +84,18 @@ final class ClassFileParser {
                 .map(parameter -> new Method.Parameter("", TypeInfo.of(parameter.getClassName())))
                 .toList();
         entityBuilder.addMethod(modifiers, name, returnType, parameters, Call.Group.EMPTY_GROUP);
-        return null;
 
-        //System.out.println("METHOD ::: " + "Access: "+ access + " Name: " + name +  "  Descriptor: " + descriptor +
-       //         " Signature: " + signature);
-        //var methodType = TypeInfo.of(signature.replace('/', '.'));
-        var parameterList = new ArrayList<String>();
-
-/*
-        var stringToFormat = descriptor;
-        var res = stringToFormat.substring(1, stringToFormat.length() - 3).split(";");
-        System.out.println(Stream.of(res).toList());
-        var startIndex = descriptor.indexOf(";");
-        /*
-        var positionIndex =
-
-
-        //var regExp = Pattern.compile("<*>*~[>;]*;");
-        //regExp.matcher(descriptor).results().forEach((matcher) -> System.out.println(matcher.group()));
-
-        var fieldType = signature == null ? decodeField(descriptor) : decodeField(signature);
-
-
-
-
-        var modifiers = Utils.toModifiers(access);
-        var group = new Call.Group(Call.Group.Kind.NONE, new ArrayList<>());
-  */
-        //System.out.println("Before type = " + descriptor);
-        var stringToFormat = descriptor.substring(1).substring(0,descriptor.length() - 3);
-        var startIndex = 0;
-        var endIndexParam = stringToFormat.indexOf(";") + 1;
-        while(endIndexParam>0){
-          var param = stringToFormat.substring(startIndex,endIndexParam);
-          //System.out.println("Param type = " + param);
-          parameterList.add(param);
-          startIndex = endIndexParam;
-          endIndexParam = stringToFormat.indexOf(";",endIndexParam + 1) + 1;
-        }
-        if(!parameterList.isEmpty()){
-          System.out.println(parameterList);
-        }
-
-
-        //var newMethod = new Method(modifiers, name, methodType, parameters, group);
         return new MethodVisitor(ASM9) {
-          private int parameterCount;
-          @Override
-          public void visitParameter(String name, int access) {
-            System.out.println(name + " " + parameterCount);
-            parameterCount++;
-          }
-
           @Override
           public void visitMethodInsn(int opcode, String owner, String name, String descriptor,
                                       boolean isInterface) {
-            //entityBuilder.addMethodCall();
-            //System.out.println("CALL :::" + "Access: " + opcode + " Owner: " + owner + " Name: " + name);
-            //System.out.println( "Descriptor: " + descriptor + " isInterface: " + isInterface);
+            var type = TypeInfo.of(owner);
+            var parametersType = Arrays.stream(Type.getArgumentTypes(descriptor))
+                    .map(parameter -> TypeInfo.of(parameter.getClassName()))
+                    .toList();
+            var returnType = TypeInfo.of(Type.getReturnType(descriptor).getClassName());
+            entityBuilder.addMethodsCallToMethod(type,name,returnType,parametersType);
           }
-
         };
       }
     }, 0);
