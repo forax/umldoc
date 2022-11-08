54df538f1d745e8829ce1a56c92c08009ea61329
brian babillon <brian.babillon@gmail.com>
2022-11-08 19:34:23 +0100

    redone for filtering packages

--- src/main/java/com/github/forax/umldoc/editor/GeneratorConfiguration.java
+++ src/main/java/com/github/forax/umldoc/editor/GeneratorConfiguration.java
@@ -1,7 +1,5 @@
 package com.github.forax.umldoc.editor;
 
-import com.github.forax.umldoc.core.Dependency;
-import com.github.forax.umldoc.core.Entity;
 import com.github.forax.umldoc.core.Package;
 import com.github.forax.umldoc.gen.Generator;
 import java.io.IOException;
@@ -12,38 +10,44 @@ import java.util.Objects;
 /**
  * Create a configuration capable of generating an uml class diagram when given a writer.
  *
- * @param entities
- *         list of entities that will be use for uml generation
- * @param dependencies
- *         list of entities that will be use for uml generation
+ * @param filterPackage
+ *         the package name that will be used for generation
  * @param generator
  *         the generator to use.
  */
-public record GeneratorConfiguration(List<Entity> entities, List<Dependency> dependencies,
-                                     Generator generator) {
+public record GeneratorConfiguration(String filterPackage, Generator generator) {
 
   /**
    * Create a configuration capable of generating an uml class diagram when given a writer.
    *
-   * @param entities
-   *         list of entities that will be use for uml generation
-   * @param dependencies
-   *         list of entities that will be use for uml generation
+   * @param filterPackage
+   *         the package name that will be used for generation
    * @param generator
    *         the generator to use.
    */
   public GeneratorConfiguration {
-    entities = List.copyOf(entities);
-    dependencies = List.copyOf(dependencies);
+    Objects.requireNonNull(filterPackage);
     Objects.requireNonNull(generator);
   }
 
   public static GeneratorConfiguration filterPackage(String packageName, Generator generator) {
-    return new GeneratorConfiguration(null, null, generator);
+    return new GeneratorConfiguration(packageName, generator);
   }
 
-  public void generate(Writer writer) throws IOException {
+  public void generate(Writer writer, List<Package> module) throws IOException {
+    var p = filterPackage(module);
+    var entities = p.entities();
+    var dependencies = p.dependencies();
     generator.generate(false, entities, dependencies, writer);
+  }
+
+  Package filterPackage(List<Package> module) {
+    return module.stream()
+                 .filter(p -> p.name()
+                               .equals(filterPackage))
+                 .findFirst()
+                 .orElseThrow(() -> new IllegalArgumentException(
+                         "package %s not found in module".formatted(filterPackage)));
   }
 
 }
