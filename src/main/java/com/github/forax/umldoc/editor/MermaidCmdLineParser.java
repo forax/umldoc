d42a6af75fff17a8a8b8e275d920b33ced48e9d9
thomas <thomas.vexiau@gmail.com>
2022-11-10 09:52:18 +0100

    CMD line parser, with options

--- src/main/java/com/github/forax/umldoc/editor/MermaidCmdLineParser.java
+++ src/main/java/com/github/forax/umldoc/editor/MermaidCmdLineParser.java
@@ -1,26 +1,25 @@
 package com.github.forax.umldoc.editor;
 
 import com.github.forax.umldoc.gen.MermaidGenerator;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
-
-
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
-public class MermaidCmdLineParser implements CommandLineParser{
+/**
+ * This class is used to parse the command line for the mermaid export.
+ *
+ */
+public class MermaidCmdLineParser implements CommandLineParser {
   private boolean hasGenerationStarted = false;
 
-
+  /**
+   * This class is used to filter the options given in the command line.
+   */
   public static class OptionsCmd implements Runnable {
-    @Option(names = "-p", description = "package Name" )
+    @Option(names = "-p", description = "package Name")
     public String packageName;
 
-    @Option(names = "-t", description = "test Name" )
+    @Option(names = "-t", description = "test Name")
     public String test;
 
 
@@ -31,12 +30,15 @@ public class MermaidCmdLineParser implements CommandLineParser{
   }
 
 
-
-
-
-
-  public Optional<GeneratorConfiguration> parseLine(String line){
-    if (isStartingLine(line)){
+  /**
+   * This method parse each line, by detecting if it start with the begin sequence :
+   *  --> "%% umldoc" for the Mermaid.
+   *
+   * @param line the line to parse
+   * @return an Optional of GeneratorConfiguration.
+   */
+  public Optional<GeneratorConfiguration> parseLine(String line) {
+    if (isStartingLine(line)) {
       this.hasGenerationStarted = true;
 
       var options = new OptionsCmd();
@@ -50,18 +52,18 @@ public class MermaidCmdLineParser implements CommandLineParser{
   }
 
 
-  public boolean endline (String line){
+  public boolean endline(String line) {
     return line.equals("```") && hasGenerationStarted; //to replace with "@enduml" for PlantUML
   }
 
-  private boolean isStartingLine(String line){
+  private boolean isStartingLine(String line) {
     return line.startsWith("%% umldoc");
   }
-
+  /*
   public static void main(String[] args) {
     var test = new MermaidCmdLineParser();
     test.parseLine("%% umldoc -p nameThomas -t test");
-  }
+  }*/
 
 
 }
