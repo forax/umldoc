aa1ee42e8a0a306f3876c574307fb88daa0e83ab
thomas <thomas.vexiau@gmail.com>
2022-11-10 09:34:34 +0100

    CMD line parser, with options

--- src/main/java/com/github/forax/umldoc/editor/MermaidCmdLineParser.java
+++ src/main/java/com/github/forax/umldoc/editor/MermaidCmdLineParser.java
@@ -1,19 +1,48 @@
 package com.github.forax.umldoc.editor;
 
+import com.github.forax.umldoc.gen.MermaidGenerator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+
+
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
 
 public class MermaidCmdLineParser implements CommandLineParser{
   private boolean hasGenerationStarted = false;
 
-  public MermaidCmdLineParser(){
 
+  public static class OptionsCmd implements Runnable {
+    @Option(names = "-p", description = "package Name" )
+    public String packageName;
+
+    @Option(names = "-t", description = "test Name" )
+    public String test;
+
+
+    @Override
+    public void run() {
+
+    }
   }
 
-  public Optional<GeneratorConfiguration> parseLine (String line){
+
+
+
+
+
+  public Optional<GeneratorConfiguration> parseLine(String line){
     if (isStartingLine(line)){
       this.hasGenerationStarted = true;
 
-
+      var options = new OptionsCmd();
+      var args = line.substring("%% umldoc ".length()).split(" ");
+      new CommandLine(options).execute(args);
+      GeneratorConfiguration.filterPackage(options.packageName, new MermaidGenerator());
       return Optional.empty();
     }
 
@@ -27,6 +56,11 @@ public class MermaidCmdLineParser implements CommandLineParser{
 
   private boolean isStartingLine(String line){
     return line.startsWith("%% umldoc");
+  }
+
+  public static void main(String[] args) {
+    var test = new MermaidCmdLineParser();
+    test.parseLine("%% umldoc -p nameThomas -t test");
   }
 
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,5 @@
 module com.github.forax.umldoc {
   requires org.objectweb.asm;
+  requires info.picocli;
+  exports com.github.forax.umldoc.editor;
 }
