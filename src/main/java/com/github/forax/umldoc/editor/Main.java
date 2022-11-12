df010870fdee610e3bfc12fcc2b38cee135f7ca6
Leo Tostain <leo.tostain@gmail.com>
2022-11-12 16:07:50 +0100

    ModuleFinder not working

---
+++ src/main/java/com/github/forax/umldoc/editor/Main.java
@@ -0,0 +1,44 @@
+package com.github.forax.umldoc.editor;
+
+import com.github.forax.umldoc.classfile.ModuleScrapper;
+
+import java.io.IOException;
+import java.lang.module.ModuleFinder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.toString(args));
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Usage : java .java path_of_markdown");
+        }
+        var finder = ModuleFinder.of(Path.of("src/main/java/"));
+        var module = finder.find("com.github.forax.umldoc");
+        if(module.isEmpty()) {
+            System.err.println("Couldn't find the Module");
+            return;
+        }
+        var packages = ModuleScrapper.scrapModule(module.get());
+
+        // "design/test.md"
+        var path = Path.of(args[0]);
+        try (
+                var reader = Files.newBufferedReader(path);
+                var writer = Files.newBufferedWriter(Path.of("resultFile.md"))
+        ) {
+            var config = new HashMap<String, CommandLineParser>();
+            config.put("mermaid", new MermaidCmdLineParser());
+            //config.put("plantuml", new PlantCmdLineParser());
+
+            /*var entities = com.github.pereiratostain.Main.asm();
+            var dependencies = new DiagramComputer(entities).buildAssociations();
+            var packages = List.of(new Package("testPackage", entities, dependencies));*/
+
+            var editor = new Editor(config, packages);
+            editor.edit(writer, reader);
+        }
+    }
+}
